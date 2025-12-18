c66b55b6aa2b74c779851b2fd4782571f214c54d
Kamal Khandal <kamalsh684@gmail.com>
2025-12-18 22:41:30 +0530

    added live link

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -7,15 +7,23 @@ export default function AppLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex h-screen overflow-hidden">
-      <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+    <div className="flex h-screen overflow-hidden relative">
+    <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
 
-      <div className="flex flex-col flex-1">
-        <Topbar onMenuClick={() => setSidebarOpen(true)} />
-        <main className="flex-1 bg-gray-100 p-4 md:p-6 overflow-y-auto">
-          <Outlet />
-        </main>
-      </div>
+    {sidebarOpen && (
+      <div
+        onClick={() => setSidebarOpen(false)}
+        className="fixed inset-0 bg-black/40 z-30 md:hidden"
+      />
+    )}
+
+    <div className="flex flex-col flex-1">
+      <Topbar onMenuClick={() => setSidebarOpen(true)} />
+      <main className="flex-1 bg-gray-100 p-4 md:p-6 overflow-y-auto">
+        <Outlet />
+      </main>
     </div>
+  </div>
+  
   );
 }
